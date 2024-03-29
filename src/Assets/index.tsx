c5f891d02f91aff3090a53bf09a1c16499c66ecc
Rafael Omodei <rafael.omodei@outlook.com>
2024-03-29 09:25:38 -0300

    feat: added images and icons

--- src/Assets/index.tsx
+++ src/Assets/index.tsx
@@ -1,2 +1,37 @@
-export const SVG = {};
-export const PNG = {};
+import addICON from './SVG/Icons/add.svg';
+import gpsICON from './SVG/Icons/gps.svg';
+import priceICON from './SVG/Icons/price.svg';
+import slectedICON from './SVG/Icons/slected.svg';
+import subtractICON from './SVG/Icons/subtract.svg';
+import subtractDesabledICON from './SVG/Icons/subtractDesabled.svg';
+import ticketICON from './SVG/Icons/ticket.svg';
+import trashICON from './SVG/Icons/trash.svg';
+import userICON from './SVG/Icons/user.svg';
+
+import aiqbrandingSVG from './SVG/aiqbranding.svg';
+import logoAiqfomeSVG from './SVG/logoAiqfome.svg';
+
+import foodPNG from './PNG/food.png';
+import matsuriPNG from './PNG/matsuri.png';
+
+export const ICON = {
+  add: addICON,
+  gps: gpsICON,
+  price: priceICON,
+  slected: slectedICON,
+  subtract: subtractICON,
+  subtractDesabled: subtractDesabledICON,
+  ticket: ticketICON,
+  trash: trashICON,
+  user: userICON,
+};
+
+export const SVG = {
+  aiqbranding: aiqbrandingSVG,
+  logoAiqfome: logoAiqfomeSVG,
+};
+
+export const PNG = {
+  food: foodPNG,
+  matsuri: matsuriPNG,
+};
